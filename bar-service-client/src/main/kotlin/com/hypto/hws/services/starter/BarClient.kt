2a48573b1d622fce527e1f66ec1ce1b257424722
ddlogesh <logeshdina@hypto.in>
2021-10-22 15:22:14 +0530

    Client - printText

--- bar-service-client/src/main/kotlin/com/hypto/hws/services/starter/BarClient.kt
+++ bar-service-client/src/main/kotlin/com/hypto/hws/services/starter/BarClient.kt
@@ -19,41 +19,20 @@ package com.hypto.hws.services.starter
 import com.hypto.hws.services.starter.BarGrpcKt.BarCoroutineStub
 import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
-import kotlinx.coroutines.delay
 import java.io.Closeable
 import java.util.concurrent.TimeUnit
 
 class BarClient(private val channel: ManagedChannel) : Closeable {
     private val stub: BarCoroutineStub = BarCoroutineStub(channel)
 
-    suspend fun orderDrink(name: String, type: DrinkType, amountInMl: Long): Bill {
-        val drink = Drink.newBuilder()
-            .setName(name)
-            .setType(type)
-            .setAmountInMl(amountInMl)
-
-        val request = DrinkRequest.newBuilder()
-            .setDrink(drink)
+    suspend fun printText(text: String): String {
+        val request = InputRequest.newBuilder()
+            .setText(text)
             .build()
 
-        println("Ordered at Bar:\n$request")
-        val response = stub.orderDrink(request)
-        println("Received from Bar:\n$response")
-
-        return response.bill
-    }
-
-    suspend fun payBill(bill: Bill, paymentAmount: Long) : Long {
-        val request = PaymentRequest.newBuilder()
-            .setBill(bill)
-            .setPaymentAmount(paymentAmount)
-            .build()
-
-        println("Paying at Bar:\n$request")
-        val response = stub.payBill(request)
-        println("Received from Bar:\n$response")
-
-        return response.balanceAmount
+        println("Input Request: $request")
+        val response = stub.printText(request)
+        return response.text
     }
 
     override fun close() {
@@ -62,56 +41,13 @@ class BarClient(private val channel: ManagedChannel) : Closeable {
 }
 
 /**
- * Bar Client orders some drinks one by one and pays the bills
- * one by one.
- *
- * Also attempts to pay a wrong bill to test the server.
+ * Print Hello World!
  */
 suspend fun main() {
     val port = 50051
-
     val channel = ManagedChannelBuilder.forAddress("localhost", port).usePlaintext().build()
-
     val client = BarClient(channel)
-    
-    var wallet: Long = 1000
 
-    val bills = mutableListOf<Bill>()
-    bills.add(
-        client.orderDrink(
-            "Johnnie Walker Blue Label",
-            DrinkType.WHISKY,
-            30)
-    )
-    delay(2000)
-
-    bills.add(
-        client.orderDrink(
-            "Absolut",
-            DrinkType.VODKA,
-            60)
-    )
-    delay(2000)
-
-    bills.add(
-        client.orderDrink(
-            "Kingfisher Ultra",
-            DrinkType.BEER,
-            300)
-    )
-    delay(2000)
-
-    bills.forEach {
-        wallet -= client.payBill(it, wallet)
-        delay(2000)
-    }
-
-    val dummyBill = Bill.newBuilder()
-        .setOrderId("123")
-        .setOrderAmount(100)
-        .build()
-    client.payBill(dummyBill, wallet)
-    delay(2000)
-
-    println("Final wallet balance is: $wallet")
+    val responseText = client.printText("Hello World!")
+    println("Output Response: $responseText")
 }
