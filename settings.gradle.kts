3f2bba9d0d0f8f4d222589ba6a7e7dc00f35cbf7
ddlogesh <logeshdina@hypto.in>
2021-10-26 14:38:17 +0530

    Rename bar-service-client to starter-service-client

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "bar-service-kotlin-client"
 
-include("bar-service-models", "bar-service-client")
+include("bar-service-models", "starter-service-client")
 
 pluginManagement {
     repositories {
