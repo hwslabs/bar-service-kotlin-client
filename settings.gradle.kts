93ba7baf16b04c4df055e2257b2c584cad142805
ddlogesh <logeshdina@hypto.in>
2021-11-19 14:09:58 +0530

    Renamed folder structure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
-rootProject.name = "{TEMPLATE_SERVICE_HYPHEN_NAME}-service-client"
+rootProject.name = "{TEMPLATE_SERVICE_HYPHEN_NAME}-service-kotlin-client"
 
-include("{TEMPLATE_SERVICE_HYPHEN_NAME}-service-models", "{TEMPLATE_SERVICE_HYPHEN_NAME}-service-kotlin-client")
+include("{TEMPLATE_SERVICE_HYPHEN_NAME}-service-models", "{TEMPLATE_SERVICE_HYPHEN_NAME}-service-client")
 
 pluginManagement {
     repositories {
